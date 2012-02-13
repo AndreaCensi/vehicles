dc711ef59a1a008fa38085f60b3f8282312b7854
Andrea Censi <andrea@cds.caltech.edu>
2012-02-12 23:25:28 -0800

    more generic code

--- src/vehicles/interfaces/world.py
+++ src/vehicles/interfaces/world.py
@@ -8,9 +8,11 @@ import datetime
 class World:
     __metaclass__ = ABCMeta
 
-    @contract(bounds='seq[3](seq[2](number))')
-    def __init__(self, bounds):
+    @contract(bounds='seq[3](seq[2](number))',
+              start_poses='None|list(seq[3](number))')
+    def __init__(self, bounds, start_poses=None):
         self.bounds = bounds
+        self.start_poses = start_poses
 
     def __repr__(self):
         return '%s' % self.__class__.__name__
@@ -42,9 +44,21 @@ class World:
         ''' Returns an episode tuple. By default it just
             samples a random pose for the robot.
         '''
-        x = np.random.uniform(self.bounds[0][0], self.bounds[0][1])
-        y = np.random.uniform(self.bounds[1][0], self.bounds[1][1])
-        th = np.random.uniform(0, np.pi * 2)
+        if self.start_poses is None:
+            x = np.random.uniform(self.bounds[0][0], self.bounds[0][1])
+            y = np.random.uniform(self.bounds[1][0], self.bounds[1][1])
+            th = np.random.uniform(0, np.pi * 2)
+        else:
+            nposes = len(self.start_poses)
+            if self.cur_pose is None:
+                self.cur_pose = np.random.randint(nposes)
+            else:
+                self.cur_pose = (self.cur_pose + 1) % nposes
+            p = self.start_poses[self.cur_pose]
+            x = p[0]
+            y = p[1]
+            th = np.deg2rad(p[2])
+
         vehicle_state = SE3_from_SE2(SE2_from_xytheta([x, y, th]))
         id_episode = unique_timestamp_string()
         return World.Episode(id_episode, vehicle_state)
